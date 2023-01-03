579eef12a3fd06311deef79edaee0bbe69e585f5
MONTEL Valentin <113093291+ValentinMontel@users.noreply.github.com>
2023-01-03 10:21:11 +0100

    Update exo2.js

--- CartoTD1/exo2.js
+++ CartoTD1/exo2.js
@@ -3,11 +3,3 @@ navigator.geolocation.getCurrentPosition(success())
 var position = "Bonjour"
 ReactDOM.render(React.createElement("h1",null,position))
 
-function success(pos) {
-    var crd = pos.coords;
-  
-    console.log('Votre position actuelle est :');
-    console.log(`Latitude : ${crd.latitude}`);
-    console.log(`Longitude : ${crd.longitude}`);
-    console.log(`La précision est de ${crd.accuracy} mètres.`);
-  }
