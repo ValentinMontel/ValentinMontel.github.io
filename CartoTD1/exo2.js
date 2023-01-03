e4e8ff137b5ee05aec9be1ce9961434a4bb25c50
MONTEL Valentin <113093291+ValentinMontel@users.noreply.github.com>
2023-01-03 10:48:38 +0100

    Update exo2.js

--- CartoTD1/exo2.js
+++ CartoTD1/exo2.js
@@ -1,4 +1,17 @@
-//navigator.geolocation.getCurrentPosition(success());
+/*navigator.geolocation.getCurrentPosition(success());*/
 
 var position = "Bonjour";
 ReactDOM.render((React.createElement("h1", null, position)), document.getElementById('Tposition'));
+
+/*if (window.DeviceOrientationEvent) {
+    window.addEventListener("deviceorientation", function(event) {
+      // alpha : rotation autour de l'axe z
+      var rotateDegrees = event.alpha;
+      // gamma : de gauche à droite
+      var leftToRight = event.gamma;
+      // bêta : mouvement avant-arrière
+      var frontToBack = event.beta;
+  
+      handleOrientationEvent(frontToBack, leftToRight, rotateDegrees);
+    }, true);
+  }*/
