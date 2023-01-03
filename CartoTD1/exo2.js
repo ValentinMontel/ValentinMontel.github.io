89d307d676c7753ee20b49dd567d62b1756f09cd
MONTEL Valentin <113093291+ValentinMontel@users.noreply.github.com>
2023-01-03 10:40:31 +0100

    Update exo2.js

--- CartoTD1/exo2.js
+++ CartoTD1/exo2.js
@@ -1,4 +1,4 @@
-navigator.geolocation.getCurrentPosition(success());
+//navigator.geolocation.getCurrentPosition(success());
 
 var position = "Bonjour";
 ReactDOM.render((React.createElement("h1", null, position)), document.getElementById('Tposition'));
