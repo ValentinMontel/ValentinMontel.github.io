851d30664fb80568ab6989a85c80a5c47ab28169
ValentinMontel <113093291+ValentinMontel@users.noreply.github.com>
2023-01-10 09:24:44 +0100

    C2Ex1

---
+++ CartoTD1/C2Ex1/exo1.js
@@ -0,0 +1,33 @@
+window.addEventListener("load", (event) => {
+  getCurrentPosition()
+})
+
+function getCurrentPosition() {
+
+  function success(position) {
+      latitude = position.coords.latitude
+      longitude = position.coords.longitude
+      setTimeout( getCurrentPosition, 5000)
+  }
+
+  function error() {
+      document.getElementById("phrase1").innerHTML = "Please active your GPS"
+  }
+
+  if (!navigator.geolocation) {
+      document.getElementById("phrase1").innerHTML = "Geolocation is not supported by your browser"
+  } else {
+      document.getElementById("phrase1").innerHTML = "Locating…"
+      navigator.geolocation.getCurrentPosition(success, error)
+  }
+}
+
+var latitude;
+var longitude;
+var map = L.map('map').setView([latitude, longitude], 13);
+/*var map = L.map('map').setView([43.83, 4.36], 13);*/
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
