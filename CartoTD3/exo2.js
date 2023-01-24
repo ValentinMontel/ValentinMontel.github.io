a541fcb78db348eeb95b22d6b7b3b3c86787f708
ValentinMontel <113093291+ValentinMontel@users.noreply.github.com>
2023-01-24 12:50:58 +0100

    Fin de cours

--- CartoTD3/exo2.js
+++
@@ -1,6 +0,0 @@
-var scene = new THREE.Scene();
-var aspect = window.innerWidth / window.innerHeight;
-var camera = new THREE.PerspectiveCamera( 75, aspect, 0.1, 1000 );
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
